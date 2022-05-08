4b9269dcfa1e70baea8e322915bf7b0a093769d1
Eugeneshv <e.kolyshev@gmail.com>
2022-05-08 12:54:46 +0300

    PP.1.1.3-4

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,8 +1,15 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.connection;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
@@ -10,26 +17,76 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
+        try (Statement statement = connection.createStatement()){
+            String SQL = "CREATE TABLE IF NOT EXISTS user (id int NOT NULL auto_increment, " +
+                    "name varchar(64), lastname varchar(64), age int, PRIMARY KEY (id))";
+            statement.execute(SQL);
 
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Statement statement = connection.createStatement()){
+            String SQL = "DROP TABLE if EXISTS user";
+            statement.execute(SQL);
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Statement statement = connection.createStatement()){
+            String SQL = new StringBuilder().append("insert into user (name, lastname, age) values ('")
+                    .append(name).append("', '").append(lastName).append("', ")
+                    .append((int) age).append(")").toString();
+            statement.execute(SQL);
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (Statement statement = connection.createStatement()){
+            String SQL = new StringBuilder().append("DELETE FROM User WHERE id=").append(id).toString();
+            statement.execute(SQL);
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String SQL = "SELECT id, name, lastname, age FROM User";
+        List<User> result = new ArrayList<>();
+        try (ResultSet rs = connection.createStatement().executeQuery(SQL)){
+            while (rs.next()) {
+                long id = (long) rs.getInt("id");
+                String name = rs.getString("name");
+                String lastname = rs.getString("lastname");
+                byte age = (byte) rs.getInt("age");
+                User user = new User();
+                user.setId(id);
+                user.setName(name);
+                user.setLastName(lastname);
+                user.setAge(age);
+                result.add(user);
+            }
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+        finally {
+            return result;
+        }
     }
 
     public void cleanUsersTable() {
-
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "TRUNCATE user";
+            statement.execute(SQL);
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USER = "root";
+    private static final String PASSWORD = "MySQLpassword";
+    public static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException
+                 | NoSuchMethodException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
